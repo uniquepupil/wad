5e9ad81739280b52052c2835b46f9dde7f3e827c
Parikshit <prikshitbhardwaj6@gmail.com>
2024-05-09 08:47:47 +0530

    .

---
+++ 6. Node(Static Website)/app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const app = express();
+
+const port = 3000;
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}/`);
+});
+
+// To start
+// node app.js
